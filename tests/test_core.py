3c4f82814af011256293adf7982c9f7ec368b42c
Franz Haas <franz.dominik.haas@gmail.com>
2024-09-28 15:18:49 +0200

    - use enum to check against enum

--- tests/test_core.py
+++ tests/test_core.py
@@ -337,7 +337,7 @@ def test_enum_issue_298():
             STX = 0x02,
         ),
         Probe(),
-        "optional" / If(lambda this: this.ctrl == "NAK", Byte),
+        "optional" / If(lambda this: this.ctrl == EnumIntegerString.new(21, 'NAK'), Byte),
     )
     common(d, b"\x15\xff", Container(ctrl='NAK', optional=255))
     common(d, b"\x02", Container(ctrl='STX', optional=None))
